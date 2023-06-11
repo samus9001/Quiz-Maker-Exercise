afc7f84f63a67c55bb69480eeab6ab2bc80e95f9
Samus9001 <sameer99@outlook.com>
2023-06-11 15:52:41 +0100

    added while loop in order for userInput to be updated

--- Program.cs
+++ Program.cs
@@ -15,78 +15,85 @@ namespace QuizMaker
 
             LogicMethods.Deserializer(QuestionsFileName, qnaList, serializer);
 
-            UIMethods.DisplayMode();
-
-            userInput = UIMethods.InputKey();
-            bool validInput = true;
-
-            if (userInput == 'N')
+            bool exit = false;
+            while (!exit)
             {
-                return;
-            }
+                UIMethods.DisplayMode();
 
-            while (userInput != 'N') // loops until the 'N' key is pressed on the Mode prompt
-            {
-                if (userInput == 'Y')
+                userInput = UIMethods.InputKey();
+                bool validInput = true;
+
+                if (userInput == 'N')
                 {
-                    QuizInformation qna = new QuizInformation();
-
-                    UIMethods.DisplayQuestionInformation();
-
-                    while (!validInput) // loops until the validInput variable is true
-                    {
-                        UIMethods.DisplayInvalidQuestion();
-
-                        qna.Question = UIMethods.InputQuestion();
-                        validInput = LogicMethods.QuestionSplit(qna);
-                    }
-
-                    qnaList.Add(qna);
-
-                    LogicMethods.Serializer(QuestionsFileName, serializer, qnaList);
+                    exit = true;
                 }
-
-                if (userInput == 'Q' && qnaList.Count > 0)
+                else
                 {
-                    while (qnaList.Count > 0)
+                    while (userInput != 'N') // loops until the 'N' key is pressed on the Mode prompt
                     {
-                        // select a random question from the list
-                        int index = random.Next(0, qnaList.Count);
-                        QuizInformation randomQuestion = qnaList[index];
-
-                        UIMethods.DisplayQuestion(randomQuestion);
-                        UIMethods.DisplayAnswers(randomQuestion);
-                        string answer = UIMethods.InputAnswer();
-                        int scoreCount = 0;
-
-                        if (answer == randomQuestion.CorrectAnswer)
+                        if (userInput == 'Y')
                         {
-                            UIMethods.DisplayCorrectAnswer();
-                            scoreCount++;
+                            QuizInformation qna = new QuizInformation();
+
+                            UIMethods.DisplayQuestionInformation();
+
+                            while (!validInput) // loops until the validInput variable is true
+                            {
+                                UIMethods.DisplayInvalidQuestion();
+
+                                qna.Question = UIMethods.InputQuestion();
+                                validInput = LogicMethods.QuestionSplit(qna);
+                            }
+
+                            qnaList.Add(qna);
+
+                            LogicMethods.Serializer(QuestionsFileName, serializer, qnaList);
                         }
-                        else
+
+                        if (userInput == 'Q' && qnaList.Count > 0)
                         {
-                            UIMethods.DisplayIncorrectAnswer(randomQuestion);
+                            while (qnaList.Count > 0)
+                            {
+                                // select a random question from the list
+                                int index = random.Next(0, qnaList.Count);
+                                QuizInformation randomQuestion = qnaList[index];
+
+                                UIMethods.DisplayQuestion(randomQuestion);
+                                UIMethods.DisplayAnswers(randomQuestion);
+                                string answer = UIMethods.InputAnswer();
+                                int scoreCount = 0;
+
+                                if (answer == randomQuestion.CorrectAnswer)
+                                {
+                                    UIMethods.DisplayCorrectAnswer();
+                                    scoreCount++;
+                                }
+                                else
+                                {
+                                    UIMethods.DisplayIncorrectAnswer(randomQuestion);
+                                }
+
+                                // remove the answered question from the qnaList
+                                qnaList.RemoveAt(index);
+
+                                UIMethods.InputPressEnterKey();
+
+                                // Check if all questions have been answered
+                                if (qnaList.Count == 0)
+                                {
+                                    UIMethods.DisplayScore(scoreCount);
+                                    UIMethods.InputPressEnterKey();
+                                    break;
+                                }
+                            }
                         }
-
-                        // remove the answered question from the qnaList
-                        qnaList.RemoveAt(index);
-
-                        UIMethods.InputPressEnterKey();
-
-                        // Check if all questions have been answered
-                        if (qnaList.Count == 0)
+                        else if (userInput == 'Q' && qnaList.Count == 0)
                         {
-                            UIMethods.DisplayScore(scoreCount);
+                            UIMethods.DisplayNoQuestionsAvailable();
                             UIMethods.InputPressEnterKey();
                             break;
                         }
                     }
-                }
-                else if (userInput == 'Q' && qnaList.Count == 0)
-                {
-                    UIMethods.DisplayNoQuestionsAvailable();
-                    UIMethods.InputPressEnterKey();
                 }
             }
         }
